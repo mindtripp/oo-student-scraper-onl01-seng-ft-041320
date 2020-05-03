3e528b4f202cac768d58c5877aaf962f4c08d86c
decent-scraper-1450 <jaimealba1574@gmail.com>
2020-05-03 21:01:31 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,8 @@
-require 'open-uri'
-require 'pry'
-
-class Scraper
-
-  def self.scrape_index_page(index_url)
-    
-  end
-
-  def self.scrape_profile_page(profile_url)
-    
-  end
-
-end
-
+Scraper.scrape_index_page(index_url)
+# => [
+        {:name => "Abby Smith", :location => "Brooklyn, NY", :profile_url => "students/abby-smith.html"},
+        {:name => "Joe Jones", :location => "Paris, France", :profile_url => "students/joe-jonas.html"},
+        {:name => "Carlos Rodriguez", :location => "New York, NY", :profile_url => "students/carlos-rodriguez.html"},
+        {:name => "Lorenzo Oro", :location => "Los Angeles, CA", :profile_url => "students/lorenzo-oro.html"},
+        {:name => "Marisa Royer", :location => "Tampa, FL", :profile_url => "students/marisa-royer.html"}
+      ]
